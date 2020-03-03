ce6046601be4dccf269472579f3a40b43a772703
Amfine <kittinut.l@ku.th>
2020-03-03 20:35:17 +0700

    fix

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,6 @@
 const config = {
   appUrl: "https://jongme.mountz.dev",
-  apiUrl: "http://jongme.mountz.dev/api",
+  apiUrl: "https://jongme.mountz.dev/api",
   fb: {
     appId: "2203330309960432",
     redirectUri:
