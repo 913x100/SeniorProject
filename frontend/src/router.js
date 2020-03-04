7080667fee486ec9b1e03bfc2ba87ad02d0ce80c
Amfine <kittinut.l@ku.th>
2020-03-04 07:36:43 +0700

    fix

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -36,7 +36,7 @@ export default new Router({
           path: "/",
           component: () => import("@/pages/Dashboard/Dashboard")
         }
-      ]
+      ],
       beforeEnter: ifAuthenicated
     },
     {
